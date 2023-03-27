fdf637d2867e580ddd6f8e3fe28365e9648926b9
Sanjith <sanjithnanthini@gmail.com>
2023-03-27 19:35:03 +0800

    TEST 0.2: COLLECTED AOA, LEVEL TOGETHER WITH CORRESPONDING IMAGES - JSON FORMAT

--- AoA Point Cloud/rtls_slave.py
+++ AoA Point Cloud/rtls_slave.py
@@ -241,30 +241,20 @@ def main1():
 
 
 #Functions to Calculate Azimuth and Elevation
-def pixel_calculate(LEVELangle,AOAangle1,AOAangle2,beta = 1):
-    #temp1 = np.pi/36
-    #temp2 = np.pi/30
-    #print("The Correction Value is： " + str(temp1))
-    #Calculate Azimuth and Elevation Angle to Radian
-    aoangle1 = AOAangle1 * np.pi/180
-    aoangle2 = AOAangle2 * np.pi/180
-    levelangle = LEVELangle * np.pi/180
+def pixel_calculate(LEVELangle,AOAangle1,AOAangle2,beta = 1,aoa_bias = 0):
 
-    cosAOAangle1 = np.cos(aoangle1)
-    cosAOAangle2 = np.cos(aoangle2)
-    coslevelangle = np.cos(levelangle)
-    sinlevelangle = np.sin(levelangle)
+    aoaangle1 = np.radians(AOAangle1+aoa_bias)
+    aoaangle2 = np.radians(AOAangle2+aoa_bias)
+    levelangle = np.radians(LEVELangle)
 
-    elvation1 = np.arccos((cosAOAangle2-coslevelangle*cosAOAangle1)/sinlevelangle)
-    sinelvation1 = np.sin(elvation1)
-    coselvation1 = np.cos(elvation1)
-    azimuth1 = np.arccos(cosAOAangle1/sinelvation1)
-    sinazimuth1 = np.sin(azimuth1)
+
+    elevation_angle = np.arccos((np.cos(aoaangle2)-np.cos(aoaangle1)*np.cos(levelangle))/np.sin(levelangle))
+    azimuth_angle = np.arccos(np.cos(aoaangle1)/np.sin(elevation_angle))
 
 
 
-    bool1 = pd.isnull(np.degrees(azimuth1))
-    bool2 = pd.isnull(np.degrees(elvation1))
+    bool1 = pd.isnull(np.degrees(azimuth_angle))
+    bool2 = pd.isnull(np.degrees(elevation_angle))
 
     if (bool1 | bool2):
         # print("The Azimuth and Elevation Angles are Incorrect")
@@ -274,8 +264,8 @@ def pixel_calculate(LEVELangle,AOAangle1,AOAangle2,beta = 1):
         pixel_size = 1/0.8  # Individual Pixel size/ micro meter
         f = beta * focal_length * 1000 / pixel_size  # Pixel Focal Length
 
-        camera_positon_u = f * (np.sin(azimuth1) / np.cos(azimuth1))  # Camera Co-ordinates -> Co-ordinate System Established by the Center of the Camera
-        camera_position_v = f * (np.cos(elvation1) / (np.cos(azimuth1) * np.sin(elvation1)))
+        camera_positon_u = f * (np.sin(azimuth_angle) / np.cos(azimuth_angle))  # Camera Co-ordinates -> Co-ordinate System Established by the Center of the Camera
+        camera_position_v = f * (np.cos(elevation_angle) / (np.cos(azimuth_angle) * np.sin(elevation_angle)))
 
 
         # Co-ordinate System Conversion: Camera co-ordinate -> GUI Co-ordinate
@@ -283,3 +273,28 @@ def pixel_calculate(LEVELangle,AOAangle1,AOAangle2,beta = 1):
         v = (1500 - camera_position_v)
 
         return (u, v)
+    
+
+def angle_calculate(LEVELangle, AOAangle1, AOAangle2):
+    """ 
+    LEVELangle: Change in level angle
+    AOAangle1: First AoA measurement
+    AOAangle2: Second AoA measurement
+    return: azimuth_angle, elevation_angle 
+    """
+    aoaangle1 = np.radians(AOAangle1)
+    aoaangle2 = np.radians(AOAangle2)
+    levelangle = np.radians(LEVELangle)
+
+    
+    elevation_angle = np.arccos(clamp((np.cos(aoaangle2)-np.cos(aoaangle1)*np.cos(levelangle))/np.sin(levelangle)))
+    azimuth_angle = np.arccos(clamp(np.cos(aoaangle1)/np.sin(elevation_angle)))
+
+    return(round(np.rad2deg(azimuth_angle),2), round(np.rad2deg(elevation_angle),2))
+
+def clamp(value):
+    if value< -1:
+        return -1
+    elif value > 1:
+        return 1
+    return value
